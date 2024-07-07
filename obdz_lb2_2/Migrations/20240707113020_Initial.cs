3ed87b5bf9edbd3f08ea4dfbfc59f533c93320f3
San4X <totalzroz@gmail.com>
2024-07-07 18:56:08 +0300

    Unversioned files

---
+++ obdz_lb2_2/Migrations/20240707113020_Initial.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace obdz_lb2_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Title = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "INTERVAL DAY(8) TO SECOND(7)", nullable: false),
+                    Genre = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Theaters",
+                columns: table => new
+                {
+                    TheaterId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "NVARCHAR2(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR2(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Theaters", x => x.TheaterId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Viewers",
+                columns: table => new
+                {
+                    ViewerId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "NVARCHAR2(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Viewers", x => x.ViewerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Screens",
+                columns: table => new
+                {
+                    ScreenId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    TheaterId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Capacity = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Screens", x => x.ScreenId);
+                    table.ForeignKey(
+                        name: "FK_Screens_Theaters_TheaterId",
+                        column: x => x.TheaterId,
+                        principalTable: "Theaters",
+                        principalColumn: "TheaterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Showtimes",
+                columns: table => new
+                {
+                    ShowtimeId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    ScreenId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    MovieId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Showtimes", x => x.ShowtimeId);
+                    table.ForeignKey(
+                        name: "FK_Showtimes_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Showtimes_Screens_ScreenId",
+                        column: x => x.ScreenId,
+                        principalTable: "Screens",
+                        principalColumn: "ScreenId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    ViewerId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    ShowtimeId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Showtimes_ShowtimeId",
+                        column: x => x.ShowtimeId,
+                        principalTable: "Showtimes",
+                        principalColumn: "ShowtimeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Viewers_ViewerId",
+                        column: x => x.ViewerId,
+                        principalTable: "Viewers",
+                        principalColumn: "ViewerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Screens_TheaterId",
+                table: "Screens",
+                column: "TheaterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Showtimes_MovieId",
+                table: "Showtimes",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Showtimes_ScreenId",
+                table: "Showtimes",
+                column: "ScreenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ShowtimeId",
+                table: "Tickets",
+                column: "ShowtimeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ViewerId",
+                table: "Tickets",
+                column: "ViewerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Showtimes");
+
+            migrationBuilder.DropTable(
+                name: "Viewers");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Screens");
+
+            migrationBuilder.DropTable(
+                name: "Theaters");
+        }
+    }
+}
---
+++ obdz_lb2_2/Migrations/20240707143254_UpdateCascadingDelete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace obdz_lb2_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCascadingDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
